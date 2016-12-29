0d7e806745e8a55b440ec6365d468f3318842010
Michael <ansolis@gmail.com>
2016-12-29 01:39:06 -0800

    Added port name in the balloon message

--- SysTrayDemo/SysTrayDemo.cpp
+++ SysTrayDemo/SysTrayDemo.cpp
@@ -9,6 +9,8 @@
 #define MAX_LOADSTRING 100
 #define	WM_USER_SHELLICON WM_USER + 1
 
+#define sizeofstr(str)   (sizeof(str) / sizeof(str[0]))
+
 // Global Variables:
 HINSTANCE hInst;	// current instance
 NOTIFYICONDATA nidApp;
@@ -25,25 +27,25 @@ LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
-                     HINSTANCE hPrevInstance,
-                     LPTSTR    lpCmdLine,
-                     int       nCmdShow)
+	HINSTANCE hPrevInstance,
+	LPTSTR    lpCmdLine,
+	int       nCmdShow)
 {
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
 
- 	// TODO: Place code here.
+	// TODO: Place code here.
 	MSG msg;
 	HACCEL hAccelTable;
 
 	// Initialize global strings
 	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
 	LoadString(hInstance, IDC_SYSTRAYDEMO, szWindowClass, MAX_LOADSTRING);
-	
+
 	MyRegisterClass(hInstance);
 
 	// Perform application initialization:
-	if (!InitInstance (hInstance, nCmdShow))
+	if (!InitInstance(hInstance, nCmdShow))
 	{
 		return FALSE;
 	}
@@ -60,7 +62,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 		}
 	}
 
-	return (int) msg.wParam;
+	return (int)msg.wParam;
 }
 
 
@@ -84,17 +86,17 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 
 	wcex.cbSize = sizeof(WNDCLASSEX);
 
-	wcex.style			= CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc	= WndProc;
-	wcex.cbClsExtra		= 0;
-	wcex.cbWndExtra		= 0;
-	wcex.hInstance		= hInstance;
-	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_SYSTRAYDEMO));
-	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
-	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
-	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_SYSTRAYDEMO);
-	wcex.lpszClassName	= szWindowClass;
-	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_SYSTRAYDEMO));
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCE(IDC_SYSTRAYDEMO);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
 	return RegisterClassEx(&wcex);
 }
@@ -111,31 +113,31 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   HWND hWnd;
-   HICON hMainIcon;
+	HWND hWnd;
+	HICON hMainIcon;
 
-   hInst = hInstance; // Store instance handle in our global variable
+	hInst = hInstance; // Store instance handle in our global variable
 
-   hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+	hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
 
-   if (!hWnd)
-   {
-      return FALSE;
-   }
+	if (!hWnd)
+	{
+		return FALSE;
+	}
 
-   hMainIcon = LoadIcon(hInstance,(LPCTSTR)MAKEINTRESOURCE(IDI_SYSTRAYDEMO)); 
+	hMainIcon = LoadIcon(hInstance, (LPCTSTR)MAKEINTRESOURCE(IDI_SYSTRAYDEMO));
 
-   nidApp.cbSize = sizeof(NOTIFYICONDATA); // sizeof the struct in bytes 
-   nidApp.hWnd = (HWND) hWnd;              //handle of the window which will process this app. messages 
-   nidApp.uID = IDI_SYSTRAYDEMO;           //ID of the icon that willl appear in the system tray 
-   nidApp.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP; //ORing of all the flags 
-   nidApp.hIcon = hMainIcon; // handle of the Icon to be displayed, obtained from LoadIcon 
-   nidApp.uCallbackMessage = WM_USER_SHELLICON; 
-   LoadString(hInstance, IDS_APPTOOLTIP,nidApp.szTip,MAX_LOADSTRING);
-   Shell_NotifyIcon(NIM_ADD, &nidApp); 
+	nidApp.cbSize = sizeof(NOTIFYICONDATA); // sizeof the struct in bytes 
+	nidApp.hWnd = (HWND)hWnd;              //handle of the window which will process this app. messages 
+	nidApp.uID = IDI_SYSTRAYDEMO;           //ID of the icon that willl appear in the system tray 
+	nidApp.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP; //ORing of all the flags 
+	nidApp.hIcon = hMainIcon; // handle of the Icon to be displayed, obtained from LoadIcon 
+	nidApp.uCallbackMessage = WM_USER_SHELLICON;
+	LoadString(hInstance, IDS_APPTOOLTIP, nidApp.szTip, MAX_LOADSTRING);
+	Shell_NotifyIcon(NIM_ADD, &nidApp);
 
-   return TRUE;
+	return TRUE;
 }
 
 void Init()
@@ -155,66 +157,66 @@ void Init()
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	int wmId, wmEvent;
-    POINT lpClickPoint;
+	POINT lpClickPoint;
 
 	switch (message)
 	{
 	case WM_CREATE:
 		break;
-		
-	case WM_USER_SHELLICON: 
+
+	case WM_USER_SHELLICON:
 		// systray msg callback 
-		switch(LOWORD(lParam)) 
-		{   
-			case WM_RBUTTONDOWN:
-				{
-					UINT uFlag = MF_BYPOSITION | MF_STRING;
-					GetCursorPos(&lpClickPoint);
-					hPopMenu = CreatePopupMenu();
-					InsertMenu(hPopMenu, 0xFFFFFFFF, MF_BYPOSITION | MF_STRING, IDM_ABOUT, _T("About"));
-					if (bDisable == TRUE)
-					{
-						uFlag |= MF_GRAYED;
-					}
-					InsertMenu(hPopMenu, 0xFFFFFFFF, uFlag, IDM_TEST2, _T("Test 2")); // Test 2
-					InsertMenu(hPopMenu, 0xFFFFFFFF, uFlag, IDM_TEST1, _T("Test 1")); // Test 1				
-					InsertMenu(hPopMenu, 0xFFFFFFFF, MF_SEPARATOR, IDM_SEP, _T("SEP"));
-					if (bDisable == TRUE)
-					{
-						InsertMenu(hPopMenu, 0xFFFFFFFF, MF_BYPOSITION | MF_STRING, IDM_ENABLE, _T("Enable"));
-					}
-					else
-					{
-						InsertMenu(hPopMenu, 0xFFFFFFFF, MF_BYPOSITION | MF_STRING, IDM_DISABLE, _T("Disable"));
-					}
-					InsertMenu(hPopMenu, 0xFFFFFFFF, MF_SEPARATOR, IDM_SEP, _T("SEP"));
-					InsertMenu(hPopMenu, 0xFFFFFFFF, MF_BYPOSITION | MF_STRING, IDM_EXIT, _T("Exit"));
+		switch (LOWORD(lParam))
+		{
+		case WM_RBUTTONDOWN:
+		{
+			UINT uFlag = MF_BYPOSITION | MF_STRING;
+			GetCursorPos(&lpClickPoint);
+			hPopMenu = CreatePopupMenu();
+			InsertMenu(hPopMenu, 0xFFFFFFFF, MF_BYPOSITION | MF_STRING, IDM_ABOUT, _T("About"));
+			if (bDisable == TRUE)
+			{
+				uFlag |= MF_GRAYED;
+			}
+			InsertMenu(hPopMenu, 0xFFFFFFFF, uFlag, IDM_TEST2, _T("Test 2")); // Test 2
+			InsertMenu(hPopMenu, 0xFFFFFFFF, uFlag, IDM_TEST1, _T("Test 1")); // Test 1				
+			InsertMenu(hPopMenu, 0xFFFFFFFF, MF_SEPARATOR, IDM_SEP, _T("SEP"));
+			if (bDisable == TRUE)
+			{
+				InsertMenu(hPopMenu, 0xFFFFFFFF, MF_BYPOSITION | MF_STRING, IDM_ENABLE, _T("Enable"));
+			}
+			else
+			{
+				InsertMenu(hPopMenu, 0xFFFFFFFF, MF_BYPOSITION | MF_STRING, IDM_DISABLE, _T("Disable"));
+			}
+			InsertMenu(hPopMenu, 0xFFFFFFFF, MF_SEPARATOR, IDM_SEP, _T("SEP"));
+			InsertMenu(hPopMenu, 0xFFFFFFFF, MF_BYPOSITION | MF_STRING, IDM_EXIT, _T("Exit"));
 
-					SetForegroundWindow(hWnd);
-					TrackPopupMenu(hPopMenu, TPM_LEFTALIGN | TPM_LEFTBUTTON | TPM_BOTTOMALIGN, lpClickPoint.x, lpClickPoint.y, 0, hWnd, NULL);
-					return TRUE;
-				}
-				break;
+			SetForegroundWindow(hWnd);
+			TrackPopupMenu(hPopMenu, TPM_LEFTALIGN | TPM_LEFTBUTTON | TPM_BOTTOMALIGN, lpClickPoint.x, lpClickPoint.y, 0, hWnd, NULL);
+			return TRUE;
+		}
+		break;
 
-			case WM_LBUTTONDOWN:
-				//MessageBox(NULL, _T("Left-click"), _T("Click"), MB_OK);
-				
-				NOTIFYICONDATA IconData = { 0 };
+		case WM_LBUTTONDOWN:
+			//MessageBox(NULL, _T("Left-click"), _T("Click"), MB_OK);
 
-				IconData.cbSize = sizeof(IconData);
-				IconData.hWnd = hWnd;
-				IconData.uFlags = NIF_INFO;
-				IconData.uID = IDI_SYSTRAYDEMO;
-				IconData.uCallbackMessage = WM_USER_SHELLICON;
-				IconData.dwInfoFlags = NIIF_INFO;
-				lstrcpy(IconData.szInfo, L"Test balloon tip");
-				lstrcpy(IconData.szInfoTitle, L"Test Title");
-				IconData.uTimeout = 15000; // in milliseconds
+			NOTIFYICONDATA IconData = { 0 };
 
-				Shell_NotifyIcon(NIM_MODIFY, &IconData);
+			IconData.cbSize = sizeof(IconData);
+			IconData.hWnd = hWnd;
+			IconData.uFlags = NIF_INFO;
+			IconData.uID = IDI_SYSTRAYDEMO;
+			IconData.uCallbackMessage = WM_USER_SHELLICON;
+			IconData.dwInfoFlags = NIIF_INFO;
+			lstrcpy(IconData.szInfo, L"Test balloon tip");
+			lstrcpy(IconData.szInfoTitle, L"Test Title");
+			IconData.uTimeout = 15000; // in milliseconds
 
-				return TRUE;
-				break;
+			Shell_NotifyIcon(NIM_MODIFY, &IconData);
+
+			return TRUE;
+			break;
 		}
 		break;
 
@@ -231,78 +233,99 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		// specified by your GUID.
 		//
 		PDEV_BROADCAST_DEVICEINTERFACE b = (PDEV_BROADCAST_DEVICEINTERFACE)lParam;
-		TCHAR strBuff[256] = { 0 };
+		TCHAR strInfo[100] = { 0 };
+		TCHAR strTitle[100] = L"Port Event";
 
 		// Output some messages to the window.
 		switch (wParam)
 		{
-			case DBT_DEVICEARRIVAL:
-				StringCchPrintf(
-					strBuff, 256,
-					TEXT("Message: DBT_DEVICEARRIVAL\n"));
-				break;
-			case DBT_DEVICEREMOVECOMPLETE:
-				StringCchPrintf(
-					strBuff, 256,
-					TEXT("Message: DBT_DEVICEREMOVECOMPLETE\n"));
-				break;
-			case DBT_DEVNODES_CHANGED:
-				StringCchPrintf(
-					strBuff, 256,
-					TEXT("Message: DBT_DEVNODES_CHANGED\n"));
-				break;
-			default:
-				StringCchPrintf(
-					strBuff, 256,
-					TEXT("Message: WM_DEVICECHANGE message received, value %d unhandled.\n"),
-					wParam);
-				break;
+		case DBT_DEVICEARRIVAL:
+			{
+				//StringCchPrintf(strInfo, sizeofstr(strInfo),
+				//	TEXT("Message: DBT_DEVICEARRIVAL\n"));
+				PDEV_BROADCAST_HDR lpdb = (PDEV_BROADCAST_HDR)lParam;
+				if (lpdb->dbch_devicetype == DBT_DEVTYP_PORT)
+				{
+					StringCchPrintf(strTitle, sizeofstr(strTitle),
+						TEXT("Port Attached\n"));
+
+					PDEV_BROADCAST_PORT lpdbp = (PDEV_BROADCAST_PORT)lpdb;
+					StringCchPrintf(strInfo, sizeofstr(strInfo),
+						TEXT("Port:  %s"), lpdbp->dbcp_name);
+				}
 			}
+			break;
+		case DBT_DEVICEREMOVECOMPLETE:
+			{
+				//StringCchPrintf(strInfo, sizeofstr(strInfo),
+				//	TEXT("Message: DBT_DEVICEREMOVECOMPLETE\n"));
+				PDEV_BROADCAST_HDR lpdb = (PDEV_BROADCAST_HDR)lParam;
+				if (lpdb->dbch_devicetype == DBT_DEVTYP_PORT)
+				{
+					StringCchPrintf(strTitle, sizeofstr(strTitle),
+						TEXT("Port Removed\n"));
 
-			NOTIFYICONDATA IconData = { 0 };
+					PDEV_BROADCAST_PORT lpdbp = (PDEV_BROADCAST_PORT)lpdb;
+					StringCchPrintf(strInfo, sizeofstr(strInfo),
+						TEXT("Port:  %s"), lpdbp->dbcp_name);
+				}
+			}
+			break;
+		//case DBT_DEVNODES_CHANGED:
+		//	StringCchPrintf(strInfo, sizeofstr(strInfo),
+		//		TEXT("Message: DBT_DEVNODES_CHANGED\n"));
+		//	break;
+		default:
+			//StringCchPrintf(strInfo, sizeofstr(strInfo),
+			//	TEXT("Message: WM_DEVICECHANGE message received, value %d unhandled.\n"), wParam);
+			//break;
+			return 0;
+		}
 
-			IconData.cbSize = sizeof(IconData);
-			IconData.hWnd = hWnd;
-			IconData.uFlags = NIF_INFO;
-			IconData.uID = IDI_SYSTRAYDEMO;
-			IconData.uCallbackMessage = WM_USER_SHELLICON;
-			IconData.dwInfoFlags = NIIF_INFO;
-			lstrcpy(IconData.szInfo, strBuff);
-			lstrcpy(IconData.szInfoTitle, L"Event Title");
-			IconData.uTimeout = 15000; // in milliseconds
+		NOTIFYICONDATA IconData = { 0 };
 
-			Shell_NotifyIcon(NIM_MODIFY, &IconData);
+		IconData.cbSize = sizeof(IconData);
+		IconData.hWnd = hWnd;
+		IconData.uFlags = NIF_INFO;
+		IconData.uID = IDI_SYSTRAYDEMO;
+		IconData.uCallbackMessage = WM_USER_SHELLICON;
+		IconData.dwInfoFlags = NIIF_INFO;
+		lstrcpy(IconData.szInfo, strInfo);
+		lstrcpy(IconData.szInfoTitle, strTitle);
+		IconData.uTimeout = 15000; // in milliseconds - this will be ignored in Win'10
+
+		Shell_NotifyIcon(NIM_MODIFY, &IconData);
 	}
 	break;
 
 
 	case WM_COMMAND:
-		wmId    = LOWORD(wParam);
+		wmId = LOWORD(wParam);
 		wmEvent = HIWORD(wParam);
 		// Parse the menu selections:
 		switch (wmId)
 		{
-			case IDM_ABOUT:
-				DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-				break;
-			case IDM_TEST1:
-				MessageBox(NULL,_T("This is a test for menu Test 1"),_T("Test 1"),MB_OK);
-				break;
-			case IDM_TEST2:
-				MessageBox(NULL,_T("This is a test for menu Test 2"),_T("Test 2"),MB_OK);
-				break;
-			case IDM_DISABLE:
-				bDisable = TRUE;
-				break;
-			case IDM_ENABLE:
-				bDisable = FALSE;
-				break;
-			case IDM_EXIT:
-				Shell_NotifyIcon(NIM_DELETE,&nidApp);
-				DestroyWindow(hWnd);
-				break;
-			default:
-				return DefWindowProc(hWnd, message, wParam, lParam);
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_TEST1:
+			MessageBox(NULL, _T("This is a test for menu Test 1"), _T("Test 1"), MB_OK);
+			break;
+		case IDM_TEST2:
+			MessageBox(NULL, _T("This is a test for menu Test 2"), _T("Test 2"), MB_OK);
+			break;
+		case IDM_DISABLE:
+			bDisable = TRUE;
+			break;
+		case IDM_ENABLE:
+			bDisable = FALSE;
+			break;
+		case IDM_EXIT:
+			Shell_NotifyIcon(NIM_DELETE, &nidApp);
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
 		}
 		break;
 		/*
@@ -328,16 +351,16 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 	UNREFERENCED_PARAMETER(lParam);
 	switch (message)
 	{
-		case WM_INITDIALOG:
-			return (INT_PTR)TRUE;
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
 
-		case WM_COMMAND:
-			if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-			{
-				EndDialog(hDlg, LOWORD(wParam));
-				return (INT_PTR)TRUE;
-			}
-			break;
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
 	}
 	return (INT_PTR)FALSE;
 }
